2a2ba835c70e138e0489ad010ca89f1ba39d7cf4
ApurwaK <meapurwa1909@gmail.com>
2024-05-15 12:44:09 +0530

    Update pp.py

--- pp.py
+++ pp.py
@@ -1,14 +1,1 @@
-import json
-
-with open("student.json", "r") as read_file:
-    print("Read JSON file")
-    student = json.load(read_file)
-
-    print("Before Pretty Printing JSON Data")
-    print(student)
-
-    print("\n")
-
-    PrettyJson = json.dumps(student, indent=4, separators=(',', ': '), sort_keys=True)
-    print("Displaying Pretty Printed JSON Data")
-    print(PrettyJson)
+print("Hello World")
