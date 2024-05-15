b74162e1339f01792dc2e35f7682d5a0b2c335c5
ApurwaK <meapurwa1909@gmail.com>
2024-05-15 13:02:19 +0530

    Update pp.py

--- pp.py
+++ pp.py
@@ -1,1 +1,14 @@
 print("Hello World")
+def validate_json_file(self,Menu.json):
+        '''
+        Ensure a given JSON file is valid.
+        :param filename: full file path
+        :return:
+        '''
+        self.log.info(f"Validating JSON file {filename}")
+        with open(filename, encoding="utf-8") as data_file:
+            file = filename.replace(self.filename, '')
+            try:
+                json.load(data_file)
+            except ValueError as error:
+                print("eroor") 
